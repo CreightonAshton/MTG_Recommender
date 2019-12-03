e092399a7320a91691a08964723b0a1e6f70fdb1
CreightonAshton <creightonashton@gmail.com>
2019-12-02 17:07:32 -0800

    fixes

--- Files/mtg_rec.py
+++ Files/mtg_rec.py
@@ -11,7 +11,7 @@ app = Flask('MTGRecommender')
 # load in the pickled dataframe
 df = pickle.load(open('../Data/filter_rec_df.pkl', 'rb'))
 
-### route 1: hello world
+### root route
 # define the route
 @app.route('/')
 # create the controller
@@ -20,44 +20,31 @@ def home():
     return render_template('form.html')
 
 
-### accept the form submission and use it to make a prediction
+### accept the form submission and use it to make recommendation
 @app.route('/submit')
 def submit():
     raw_data = request.args
-    print(raw_data)
+
     # format the user input
     user_input = raw_data['card']
     form = raw_data['format']
 
-    # load in the pickled dataframe
-    #df = pickle.load(open('../Data/filter_rec_df.pkl', 'rb'))
-
     # make recommendation based on the user data
-    rec_cards = list(df.loc[df['legalities_' + form] == 1][user_input].sort_values()[1:11].index)
-    card1 = rec_cards[0]
-    card2 = rec_cards[1]
-    card3 = rec_cards[2]
-    card4 = rec_cards[3]
-    card5 = rec_cards[4]
-    card6 = rec_cards[5]
-    card7 = rec_cards[6]
-    card8 = rec_cards[7]
-    card9 = rec_cards[8]
-    card10 = rec_cards[9]
-    card1_link = df.loc[card1, 'card_link']
-    card2_link = df.loc[card2, 'card_link']
-    card3_link = df.loc[card3, 'card_link']
-    card4_link = df.loc[card4, 'card_link']
-    card5_link = df.loc[card5, 'card_link']
-    card6_link = df.loc[card6, 'card_link']
-    card7_link = df.loc[card7, 'card_link']
-    card8_link = df.loc[card8, 'card_link']
-    card9_link = df.loc[card9, 'card_link']
-    card10_link = df.loc[card10, 'card_link']
-    print(card1_link)
+    rec_cards = list(df.loc[df['legalities_' + form] == 1][user_input].sort_values()[0:11].index)
+
+    # error checking to see if the user-inputted card is the same as the first recommended card
+    if rec_cards[0] == user_input:
+        rec_cards = rec_cards[1:11]
+    else:
+        rec_cards = rec_cards[0:10]
+
+    # get the links for the recommended cards
+    links = []
+    for card in rec_cards:
+        links.append(df.loc[card, 'card_link'])
 
     # return the prediction in the template
-    return render_template('results.html', rec_cards_1 = card1, rec_cards_2 = card2, rec_cards_3 = card3, rec_cards_4 = card4, rec_cards_5 = card5, rec_cards_6 = card6, rec_cards_7 = card7, rec_cards_8 = card8, rec_cards_9 = card9, rec_cards_10 = card10, card1_link = card1_link, card2_link = card2_link, card3_link = card3_link, card4_link = card4_link, card5_link = card5_link, card6_link = card6_link, card7_link = card7_link, card8_link = card8_link, card9_link = card9_link, card10_link = card10_link)
+    return render_template('results.html', len = len(rec_cards), rec_cards = rec_cards, links = links)
 
 # run the app
 if __name__ == "__main__":
